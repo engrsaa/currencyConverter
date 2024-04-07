a7edc54d1976e13ffcbe31140a2080d334c3f374
Syed Asad Abbas <159759177+engrsaa@users.noreply.github.com>
2024-04-07 12:25:06 +0500

    initaial comment

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const currencies = { USD: 1, EUR: 0.92, GBP: 0.79, AED: 3.67, PKR: 278 };
+const userInput = await inquirer.prompt([
+    {
+        name: 'fromCurrency', message: 'Select From Currency: ', type: "list", choices: ['USD', 'EUR', 'GBP', 'AED', 'PKR']
+    },
+    {
+        name: 'toCurrency', message: 'Select To Currency: ', type: "list", choices: ['USD', 'EUR', 'GBP', 'AED', 'PKR']
+    },
+    {
+        name: 'amount', message: 'Enter Amount to convert:  ', type: "number"
+    }
+]);
+let fromCurrency = currencies[userInput.fromCurrency], toCurrency = currencies[userInput.toCurrency], amountInUSD = userInput.amount / fromCurrency, result = amountInUSD * toCurrency;
+console.log(`By converting ${userInput.fromCurrency} ${userInput.amount} in ${userInput.toCurrency} you get ${userInput.toCurrency} ${result}`);
